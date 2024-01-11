e9944d021faf143c2fea99ee63ce17e516a7c8ea
yashsachdev <yashsachdev5677@gmail.com>
2024-01-12 00:35:00 +0530

    array

--- portfolio-3/Array/Array/Program.cs
+++ portfolio-3/Array/Array/Program.cs
@@ -20,6 +20,39 @@ static void CheckLargest(string[] arr, int n, string k)
         Console.WriteLine($"Array is Full: {arr.Length}");
     }
 }
+static void CheckSecondLargest(int[] arr, int n)
+{
+    int first = int.MinValue; 
+    int second = int.MinValue;
+    //1 . if aa[i] > first then
+    //first = a[i];
+    //second = first;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > first)
+        {
+            second = first;
+            first = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != first)
+        {
+            second = arr[i];
+        }
+    }
+    if (second == int.MinValue)
+        Console.WriteLine("There is no second largest"
+                        + " element\n"); 
+        else
+        Console.WriteLine("The second largest element"
+                        + " is " + second);
+    // 2. if first <=a[i] <= second
+    //second = a[i] ;
+}
+
+
+int[] arr = new int[6] { 12, 35, 1, 10, 34, 1 };
+int n = arr.Length;
+CheckSecondLargest(arr, n);
 
 Console.WriteLine("Hello Mono World");
 string[] animals = new string[3];
